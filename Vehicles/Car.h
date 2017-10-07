dad1cfeaf5a97e3b58aaa2d9ee38623b8fc9eadd
Carlo <carlo.namoca@gmail.com>
2017-10-07 16:41:46 -0700

    finish inheritance

---
+++ Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Carlo Namoca on 2017-10-07.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
